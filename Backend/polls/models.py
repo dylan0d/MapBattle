450a7f3eb07cf9adde5090fa80fd161a43cbb2f8
Dylan O'Driscoll <dylan.odriscoll@gmail.com>
2017-09-21 16:37:48 +0100

    Info now comes from database instead of hardcoding

--- Backend/polls/models.py
+++ Backend/polls/models.py
@@ -12,9 +12,14 @@ class Question(models.Model):
 
 
 
-class Choice(models.Model):
+class Projection(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null = True)
+    description = models.CharField(max_length=2000)
+    personality = models.CharField(max_length=2000)
+    credit = models.CharField(max_length=2000, null = True)
+    creditURL = models.CharField(max_length=2000, null = True)
+    image_path = models.CharField(max_length=2000, null = True)
     votes = models.IntegerField(default=0)
     def __str__(self):
-        return self.choice_text
+        return self.name
